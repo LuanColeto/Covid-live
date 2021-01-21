7e731aa586d2c9b7388ee067c43441623a696214
Luan Coleto <luancoleto@outlook.com>
2021-01-21 20:27:20 -0300

    Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  :hover {
+    text-decoration: none;
+  }
+`
+
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer pt-0">
+            <div className="row align-items-center justify-content-lg-between">
+                <div className="col-lg-6">
+                    <div className="copyright text-center  text-lg-left  text-muted">
+                        &copy; 2020 <a href="https://www.creative-tim.com" className="font-weight-bold ml-1"
+                                       target="_blank">Covid live</a>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <ul className="nav nav-footer justify-content-center justify-content-lg-end">
+                        <li className="nav-item">
+                            <a href="https://www.creative-tim.com" className="nav-link" target="_blank">Sobre nós</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="https://documenter.getpostman.com/view/10808728/SzS8rjbc" className="nav-link"
+                               target="_blank">API utilizada</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer
